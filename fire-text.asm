8fa3ae5129363cfb360bd79632fbd318d5c5317e
Jiri Vrba <jiri.vrba@firma.seznam.cz>
2024-02-13 22:30:47 +0100

    compiles

--- fire-text.asm
+++ fire-text.asm
@@ -12,16 +12,15 @@
         ;; you can think of. Use it at your own risk.                 ;;
         ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-.386
-tsm 	segment byte public use16
-        assume cs:tsm,ds:tsm,ss:tsm,es:tsm
         org 100h
+section .text
+
 beg:    mov ax,1130h                    ;startup section
         mov bh,2
         int 10h
         push es
         pop fs
-        mov font,bp
+        mov [font],bp
         mov ax,13h
         int 10h
         xor cx,cx
@@ -92,11 +91,11 @@ ll6:    xor ax,ax
         push dx
         mov ax,3508h
         int 21h
-        mov word ptr oldint,bx
-        mov word ptr oldint+2,es
+        mov [oldint],bx
+        mov [oldint+2],es
         push 0a000h
         pop es
-        mov dx,offset timer
+        mov dx,timer
         mov ax,2508h
         int 21h
 
@@ -104,37 +103,37 @@ ll6:    xor ax,ax
 l1:     mov cx,320
 l2:     mov di,19520
         sub di,cx
-l3:     movzx ax,byte ptr es:[di]
-        add al,es:[di]+2
+l3:     movzx ax,byte [es:di]
+        add al,[es:di+2]
         adc ah,bh
-        add al,es:[di]-2
+        add al,[es:di-2]
         adc ah,bh
-        add al,es:[di]+642
+        add al,[es:di+642]
         adc ah,bh
         shr ax,2
         jz short l4
         dec ax
 l4:     mov ah,al
-        mov es:[di]-640,ax
-        mov es:[di]-320,ax
+        mov [es:di-640],ax
+        mov [es:di-320],ax
         add di,bp
         jnc l3
         sub di,bp
         pop ax
-        lea ax,short [eax+eax*2]        ;one opcode "RAND"
+        lea ax,word [eax+eax*2]        ;one opcode "RAND"
         push ax
-        mov es:[di],bh
+        mov [es:di],bh
         and ah,00100000b
         jz short l5
-        mov byte ptr es:[di],160
+        mov byte [es:di],160
 l5:     dec cx
         loop l2
         in al,60h
         cmp al,1
         jne short l1
 
-        mov dx,word ptr oldint          ;cleanup section
-        mov ds,word ptr oldint+2
+        mov dx,[oldint]          ;cleanup section
+        mov ds,[oldint+2]
         mov ax,2508h
         int 21h
         mov ax,3
@@ -142,20 +141,20 @@ l5:     dec cx
         pop dx
         ret
 
-timer   proc far                        ;interrupt handler
+timer:   ;PROC FAR                        ;interrupt handler
         pusha
         mov cl,intext
         cmp cl,8
         jne short xl1
-        inc poradi
-        mov byte ptr intext,0
-xl1:    mov di,offset text
+        inc byte [poradi]
+        mov byte [intext],0
+xl1:    mov di,text
         add di,poradi
-        cmp byte ptr ds:[di],0
+        cmp byte [ds:di],0
         jne short xl2
         sub di,poradi
-        mov poradi,0
-xl2:    mov al,ds:[di]
+        mov byte [poradi],0
+xl2:    mov al,[ds:di]
         mov dl,14
         mul dl
         mov di,font
@@ -164,32 +163,31 @@ xl2:    mov al,ds:[di]
         mov cx,14
 xl3:    push cx
         mov bx,157
-xl4:    mov ax,es:[si+2]
+xl4:    mov ax,[es:si+2]
         mov es:[si],ax
         inc si
         inc si
         dec bx
         jnz short xl4
-        mov ah,fs:[di]
+        mov ah,[fs:di]
         inc di
         mov cl,intext
         shl ah,cl
         test ah,10000000b
         jz short xl5
         mov ax,819eh
-        add ah,color
-        mov es:[si-2],ax
-        inc color
-        cmp color,62
+        add ah,byte [color]
+        mov [es:si-2],ax
+        inc byte [color]
+        cmp byte [color],62
         jnz short xl5
-        mov color,0
+        mov byte [color],0
 xl5:    add si,326
         pop cx
         loop xl3
-        inc intext
+        inc byte [intext]
         popa
-        jmp oldint
-timer   endp
+        jmp [oldint]
 
 color   db 0                            ;data section
 intext  db 0
@@ -198,5 +196,3 @@ font    dw 0
 oldint  dd 0
 text    db '--* Greeting from TASeMnik',39,'s_/\_/\_/=8> Manufactory *-- '
         db '                      ',0
-tsm     ends
-        end     beg
